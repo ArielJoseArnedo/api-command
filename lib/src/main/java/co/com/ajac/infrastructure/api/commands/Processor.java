b3b28414309eca72b3d3440d12ea2d0a19a450b5
Ariel Arnedo <arielarnedo@seven4n.com>
2021-06-17 22:27:19 -0500

    se modifica el processor

--- lib/src/main/java/co/com/ajac/infrastructure/api/commands/Processor.java
+++ lib/src/main/java/co/com/ajac/infrastructure/api/commands/Processor.java
@@ -2,7 +2,7 @@ package co.com.ajac.infrastructure.api.commands;
 
 import co.com.ajac.infrastructure.api.events.EventPublisher;
 
-import java.util.List;
+import io.vavr.collection.List;
 
 public interface Processor {
     List<CommandProvider> commandProviders();
