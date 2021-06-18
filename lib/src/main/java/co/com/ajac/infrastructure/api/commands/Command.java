17db48637cb11a0839cf4035b69342e451eb8e40
Ariel Arnedo <arielarnedo@seven4n.com>
2021-06-18 16:00:55 -0500

    se modifica el error de comando

--- lib/src/main/java/co/com/ajac/infrastructure/api/commands/Command.java
+++ lib/src/main/java/co/com/ajac/infrastructure/api/commands/Command.java
@@ -9,5 +9,5 @@ import io.vavr.control.Option;
 
 
 public interface Command{
-    default FutureEither<AppError, Tuple2<Option<Response>, List<Event>>> execute(Request request) { return FutureEither.left(CommandError.COMMAND_NOT_IMPLEMENT); }
+    default FutureEither<AppError, Tuple2<Option<Response>, List<Event>>> execute(Request request) { return FutureEither.left(CommandError.COMMAND_NOT_IMPLEMENTED); }
 }
--- lib/src/main/java/co/com/ajac/infrastructure/api/commands/CommandError.java
+++ lib/src/main/java/co/com/ajac/infrastructure/api/commands/CommandError.java
@@ -3,7 +3,7 @@ package co.com.ajac.infrastructure.api.commands;
 import co.com.ajac.domain.errors.AppError;
 
 public enum CommandError implements AppError {
-    COMMAND_NOT_IMPLEMENT("", "", "");
+    COMMAND_NOT_IMPLEMENTED("EIC-1", "Command not implmented", "Command has no implementation, so it cannot be executed.");
 
     private final String code;
     private final String message;
