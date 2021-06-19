a927e3f624d82b48cb6a69e2fd6508d0ac659cdd
Ariel Arnedo <arielarnedo@seven4n.com>
2021-06-19 17:19:41 -0500

    se actualiza los errores de comando

--- lib/src/main/java/co/com/ajac/infrastructure/api/commands/CommandError.java
+++ lib/src/main/java/co/com/ajac/infrastructure/api/commands/CommandError.java
@@ -3,7 +3,9 @@ package co.com.ajac.infrastructure.api.commands;
 import co.com.ajac.domain.errors.AppError;
 
 public enum CommandError implements AppError {
-    COMMAND_NOT_IMPLEMENTED("EIC-1", "Command not implmented", "Command has no implementation, so it cannot be executed.");
+    COMMAND_NOT_IMPLEMENTED("EIC-1", "Command not implmented", "Command has no implementation, so it cannot be executed."),
+    COMMAND_PROVIDER_NOT_FOUND("EIC-2", "Command provider not found", "No command provider was found with this version."),
+    COMMAND_NOT_FOUND("EIC-3", "Command not found", "Name of this command was not found for the specified command provider.");
 
     private final String code;
     private final String message;
