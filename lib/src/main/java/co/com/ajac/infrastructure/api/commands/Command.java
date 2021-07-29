8dc6984649b9d5981fdb48b7108eb3999e4fffdb
Ariel Arnedo <arielarnedo@seven4n.com>
2021-07-28 22:16:05 -0500

    se actualiza la version del base

--- lib/src/main/java/co/com/ajac/infrastructure/api/commands/Command.java
+++ lib/src/main/java/co/com/ajac/infrastructure/api/commands/Command.java
@@ -1,12 +1,13 @@
 package co.com.ajac.infrastructure.api.commands;
 
+import co.com.ajac.base.errors.AppError;
+import co.com.ajac.base.events.Event;
 import co.com.ajac.concurrency.FutureEither;
-import co.com.ajac.domain.errors.AppError;
-import co.com.ajac.infrastructure.api.events.Event;
+
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
 
-public interface Command{
+public interface Command {
     default FutureEither<AppError, Tuple2<Option<Response>, List<Event>>> execute(Request request) { return FutureEither.left(CommandError.COMMAND_NOT_IMPLEMENTED); }
 }
